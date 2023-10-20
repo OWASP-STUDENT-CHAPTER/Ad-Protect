062a92026898df21ae2bc91cdd280f44dd98e931
MJ-NJ <mjha_be21@thapar.edu>
2023-10-20 14:33:54 +0530

    Added basic adblocker

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.webRequest.onBeforeRequest.addListner(
+    function(details){ return { cancel:true}},
+    {urls: filter },
+    ["blocking"]
+)
+
+filter = [
+    "*://*.doubleclick.net/*",
+    "*://partner.googleadservices.com/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.google-analytics.com/*",
+    "*://creative.ak.fbcdn.net/*",
+    "*://*.adbrite.com/*",
+    "*://*.exponential.com/*",
+    "*://*.quantserve.com/*",
+    "*://*.scorecardresearch.com/*",
+    "*://*.zedo.com/*",
+]
