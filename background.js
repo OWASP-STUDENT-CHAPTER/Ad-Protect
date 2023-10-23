9064a9e78b596d71e64a852fa17b7ff7baac7241
Sanya <sgupta12_be21@thapar.edu>
2023-10-23 14:47:46 +0530

    Basic changes for ad blocking made

--- background.js
+++ background.js
@@ -1,18 +1,5 @@
-chrome.webRequest.onBeforeRequest.addListner(
-    function(details){ return { cancel:true}},
-    {urls: filter },
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: ["*://*.https://www.youtube.com/*"] },
     ["blocking"]
 )
-
-filter = [
-    "*://*.doubleclick.net/*",
-    "*://partner.googleadservices.com/*",
-    "*://*.googlesyndication.com/*",
-    "*://*.google-analytics.com/*",
-    "*://creative.ak.fbcdn.net/*",
-    "*://*.adbrite.com/*",
-    "*://*.exponential.com/*",
-    "*://*.quantserve.com/*",
-    "*://*.scorecardresearch.com/*",
-    "*://*.zedo.com/*",
-]
